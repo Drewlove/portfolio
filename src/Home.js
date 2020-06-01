991b01956859f8c6f1a2ec8932c652e09131babc
Drew Love <davidandrewlove84@gmail.com>
2020-06-01 11:02:56 -0400

    Skills section and Projects section has same margin-top when viewed on iPad

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import Face from "./_Common/Face";
 const Home = () => {
   return (
       <Face>
-        <h1>Hi! I'm Drew</h1>
+        <h1>Hi! I'm Drew.</h1>
         <p>I make full stack web applications.</p>
       </Face>
   );

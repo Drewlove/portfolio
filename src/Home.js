953928f9d5dca534782567626b1e72858a5813d2
Drew Love <davidandrewlove84@gmail.com>
2020-06-01 09:05:56 -0400

    Adjust dimensions of face and word bubble arrow for accurate mobile display

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
         <div class="word-bubble-arrow"></div>
       </div>
 
+      <div class='face-container'></div>
       <div class="face">
         <div class="haircut hair"></div>
         <div class="face-top">
@@ -41,20 +42,20 @@ const Home = () => {
         </div>
       </div>
     </section>
-    // <section className='section-about'>
-    // <MockMobile>
-    // <h2>Drew Love</h2>
-    // <h3>Full Stack Engineer</h3>
-    // <div class='mock-mobile-about-container'>
-    //     <div class='mock-mobile-about-profile-pic'></div>
-    //     <ul class='mock-mobile-unordered-list'>
-    //       <li></li>
-    //       <li></li>
-    //     </ul>
-    //     <div class='mock-mobile-dummy-text-box'></div>
-    //   </div>
-    // </MockMobile>
-    // </section>
+    // {/* // <section className='section-about'>
+    // // <MockMobile>
+    // // <h2>Drew Love</h2>
+    // // <h3>Full Stack Engineer</h3>
+    // // <div class='mock-mobile-about-container'>
+    // //     <div class='mock-mobile-about-profile-pic'></div>
+    // //     <ul class='mock-mobile-unordered-list'>
+    // //       <li></li>
+    // //       <li></li>
+    // //     </ul>
+    // //     <div class='mock-mobile-dummy-text-box'></div>
+    // //   </div>
+    // // </MockMobile>
+    // // </section> */}
   );
 };
 
